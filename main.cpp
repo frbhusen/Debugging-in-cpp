b954b686f54f3dabd1a4b7dad58cacbec9fd49dd
frbhusen <131530324+frbhusen@users.noreply.github.com>
2024-01-07 14:44:26 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,19 @@
 using namespace std;
 
 #ifndef ONLINE_JUDGE
-#include <custom_debug.h>;
+#include <Debug.h>;
 #else
 #define debug(...) 4
 #endif
 
 int main(){
   // for example:
-  int n;
-  cin>>n;
-  debug(n);
+  
+  int a = 5;
+  bool b = 1;
+  char c = 'c';
+  double d = 1.52;
+  vevctor<int>e = {1,2,3,4};
+  debug(a, b, c, d, e);
+
 }

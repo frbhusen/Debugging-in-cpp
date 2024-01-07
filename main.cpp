862ca26bee8568c9e42e7fb668447967a4fc7e68
frbhusen <131530324+frbhusen@users.noreply.github.com>
2024-01-07 14:41:44 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include <custom_debug.h>;
+#else
+#define debug(...) 4
+#endif
+
+int main(){
+  // for example:
+  int n;
+  cin>>n;
+  debug(n);
+}
